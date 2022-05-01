46101aa1432d2e3234a542702c4cd55c6f125463
面向搜索引擎编程 <mapno1.com@gmail.com>
2022-05-01 17:39:58 +0800

    Update Program.cs
    更新 支持串口连接, 通过USB数据线发送信息

--- 客户端/Program.cs
+++ 客户端/Program.cs
@@ -7,12 +7,12 @@ using System.Text;
 using System.Net;
 using OpenHardwareMonitor.Hardware;
 using System.Collections.Generic;
+using System.IO.Ports;
 
 namespace ConsoleApp1
 {
     public class Class1
     {
-        static double getAllRam = 0;
         static double useVoltage = 0;
         static int updateTime = 0;
         static int sendValueID_1 = 0;
@@ -29,22 +29,47 @@ namespace ConsoleApp1
         static double gpuLoad = 0;
         static double gpuRamLoad = 0;
         static double gpuTemperature = 0;
+
+        static IPAddress remoteIp;
+        static IPEndPoint remotePoint;
+
+        static SerialPort serialPort = new SerialPort();
+        static bool isWiredWireless = false;
+        static string serialPortIndex = "";
+        static int baudRateValue = 115200;
+
         static void Main(string[] args)
         {
            
             if (args.Length != 6)
             {
                 Console.WriteLine("输入的配置项不正确!\n");
-                Console.WriteLine("右键CpuRamGet.exe -> 创建快捷方式 -> 快捷方式右键属性 -> 目标 -> CpuRamGet.exe 尾部依照下列说明添加\n");
-                Console.WriteLine("1.IP  2.端口号  3.电压表最大值(3.3V以内)  4.刷新时间(单位毫秒)  5.发送数据ID 1  6.发送数据ID 2\n");
-                Console.WriteLine("可选的ID(6选2):  1 CPU使用率, 2 CPU温度, 3 内存使用率, 4 GPU使用率, 5 GPU显存占用, 6 GPU温度\n");
-                Console.WriteLine("每项中间空格区分\n \n例: 192.168.1.199 2333 1 500 1 2");
+                Console.WriteLine("【1】右键CpuRamGet.exe -> 创建快捷方式 -> 快捷方式右键属性 -> 目标 -> CpuRamGet.exe 尾部依照下列说明添加\n");
+                Console.WriteLine("【2】1.IP/串口号  2.端口号/波特率  3.电压表最大值(3.3V以内)  4.刷新时间(单位毫秒)  5.发送数据ID_1  6.发送数据ID_2\n");
+                Console.WriteLine("【3】可选的ID(6选2):  1 CPU使用率, 2 CPU温度, 3 内存使用率, 4 GPU使用率, 5 GPU显存占用, 6 GPU温度\n");
+                Console.WriteLine("注意:每项中间空格区分\n");
+                Console.WriteLine("\n无线示例: 192.168.1.199 2333 1 500 1 2");
+                Console.WriteLine("\nUSB串口示例: com4 115200 1 500 1 2");
                 Console.ReadKey();
                 return;
             }
+            char[] switchString = "com".ToCharArray();
+            char[] switchString2 = "COM".ToCharArray();
+            int inquiryIndex = args[0].ToString().IndexOfAny(switchString);
+            int inquiryIndex2 = args[0].ToString().IndexOfAny(switchString2);
+            
+            if (inquiryIndex == 0 || inquiryIndex2 == 0)
+            {
+                serialPortIndex = args[0];
+                baudRateValue = Convert.ToInt32(args[1]);
+                isWiredWireless = true;
+            }
+            else
+            {
+                esp32Ip = args[0];
+                esp32Port = Convert.ToInt32(args[1]);
+            }
 
-            esp32Ip = args[0];
-            esp32Port = Convert.ToInt32(args[1]);
             useVoltage = (255.0f / 3.30f) * Convert.ToDouble(args[2]);
             updateTime = Convert.ToInt32(args[3]);
             sendValueID_1 = Convert.ToInt32(args[4]);
@@ -117,16 +142,13 @@ namespace ConsoleApp1
                             {
                                 if (sensor != null)
                                 {
-                                    if (sensor != null)
+                                    if (sensor.Index == 0) //总占用率
                                     {
-                                        if (sensor.Index == 0) //总占用率
-                                        {
-                                            gpuLoad = (double)sensor.Value;
-                                        }
-                                        else if (sensor.Index == 4) //显存占用
-                                        {
-                                            gpuRamLoad = (double)sensor.Value;
-                                        }
+                                        gpuLoad = (double)sensor.Value;
+                                    }
+                                    else if (sensor.Index == 4) //显存占用
+                                    {
+                                        gpuRamLoad = (double)sensor.Value;
                                     }
                                 }
                             }
@@ -172,7 +194,6 @@ namespace ConsoleApp1
                             }
                         }
                     }
-                    
                 }
 
                 cpuLoad = Math.Round(cpuLoad,0);
@@ -183,15 +204,14 @@ namespace ConsoleApp1
                 gpuTemperature = Math.Round(gpuTemperature, 0);
 
                 Console.WriteLine("===================bilibili日出东水===================\n");
-                Console.WriteLine("ID_1  CPU 使用率: " + cpuLoad + " %\n");
-                Console.WriteLine("ID_2  CPU 温度: " + cpuTemperature + " C\n");
+                Console.WriteLine("ID_1  CPU 使用率: {0} %\n", cpuLoad);
+                Console.WriteLine("ID_2  CPU 温度: {0} C\n", cpuTemperature);
 
-                //Console.WriteLine("总内存: " + getAllRam + " MB");
-                Console.WriteLine("ID_3  内存使用率: " + ramLoad + " %\n");
+                Console.WriteLine("ID_3  内存使用率: {0} %\n", ramLoad);
 
-                Console.WriteLine("ID_4  GPU 使用率: " + gpuLoad + " %\n");
-                Console.WriteLine("ID_5  GPU 显存占用: " + gpuRamLoad + " %\n");
-                Console.WriteLine("ID_6  GPU 温度: " + gpuTemperature + " C\n");
+                Console.WriteLine("ID_4  GPU 使用率: {0} %\n", gpuLoad);
+                Console.WriteLine("ID_5  GPU 显存占用: {0} %\n", gpuRamLoad);
+                Console.WriteLine("ID_6  GPU 温度: {0}  C\n", gpuTemperature);
 
                 //dac的数值范围为0-255,实际输出电压值为0-3.3v
                 string _sendType_1;
@@ -199,7 +219,7 @@ namespace ConsoleApp1
                 double _sendValue_1 =  SwitchSendValue(sendValueID_1, out _sendType_1);
                 double _sendValue_2 = SwitchSendValue(sendValueID_2, out _sendType_2);
 
-                Console.WriteLine("配置的数据类型: [" + _sendType_1 +"] / ["+ _sendType_2+"]\n");
+                Console.WriteLine("配置的数据类型: [{0}] / [{1}] \n",_sendType_1,_sendType_2);
 
                 string sendStr1 = Convert.ToString(Math.Round(RamapValue(_sendValue_1, 0, 100.0, 0, useVoltage), 0));
                 string sendStr2 = Convert.ToString(Math.Round(RamapValue(_sendValue_2, 0, 100.0, 0, useVoltage), 0));
@@ -238,37 +258,73 @@ namespace ConsoleApp1
 
         static void Init()
         {
+            if (isWiredWireless)
+            {
+                serialPort.PortName = serialPortIndex;//串口号
+                serialPort.BaudRate = baudRateValue;//波特率
+                serialPort.DataBits = 8;//数据位
+                try
+                {
+                    serialPort.Open();
+                }
+                catch (Exception _exception)
+                {
+                    Console.WriteLine(_exception.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    remoteIp = IPAddress.Parse(esp32Ip);
+                    remotePoint = new IPEndPoint(remoteIp, esp32Port);
+                }
+                catch (Exception _exception)
+                {
+                    Console.WriteLine(_exception.Message);
+                }
+
+            }
+
             //获取Cpu,Gpu使用率温度等
             computer = new Computer();
             computer.CPUEnabled = true;
             computer.RAMEnabled = true;
             computer.GPUEnabled = true;
             computer.Open();
-
-            //获取总物理内存大小
-            ManagementClass cimobject1 = new ManagementClass("Win32_PhysicalMemory");
-            ManagementObjectCollection moc1 = cimobject1.GetInstances();
-            double capacity = 0;
-            foreach (ManagementObject mo1 in moc1)
-            {
-                capacity += ((Math.Round(Int64.Parse(mo1.Properties["Capacity"].Value.ToString()) / 1024 / 1024 / 1024.0, 1)));
-            }
-            moc1.Dispose();
-            cimobject1.Dispose();
-            getAllRam = capacity * 1024;
         }
 
         static void Esp32Connected(string message)
         {
-            //UDP 方式发送信息给开发板
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-
-            if (udpClient != null)
+            if (isWiredWireless)
             {
-                IPAddress remoteIp = IPAddress.Parse(esp32Ip);
-                IPEndPoint remotePoint = new IPEndPoint(remoteIp, esp32Port);
-                udpClient.Send(buffer, buffer.Length, remotePoint);
-                Console.WriteLine("IP:" + esp32Ip + " 端口:" + esp32Port + " 发送信息: " + message);
+                if(serialPort.IsOpen)
+                {
+                    serialPort.WriteLine(message);
+                    Console.WriteLine("串口号: {0} 波特率: {1} 发送信息: {2}", serialPortIndex, baudRateValue, message);
+                }
+                else
+                {
+                    Console.WriteLine("出错啦! 串口: {0} 无法打开,请检查!", serialPortIndex);
+                }
+            }
+            else
+            {
+                //UDP 方式发送信息给开发板
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+                if (udpClient != null)
+                {
+                    if (remotePoint !=null)
+                    {
+                        udpClient.Send(buffer, buffer.Length, remotePoint);
+                        Console.WriteLine("IP: {0} 端口: {1} 发送信息: {2}", esp32Ip, esp32Port, message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("出错啦! IP地址: {0} 无效,请检查!", esp32Ip);
+                    }
+                }
             }
         }
     }
